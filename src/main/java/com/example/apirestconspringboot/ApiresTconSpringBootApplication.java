4f3331a4ca6dac2ac1a976503f1fd28126bfa9aa
Axel Adán <82789056+AxelAdan@users.noreply.github.com>
2022-04-02 21:06:52 -0600

    Creación de proyecto

---
+++ src/main/java/com/example/apirestconspringboot/ApiresTconSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.apirestconspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiresTconSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiresTconSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apirestconspringboot/ApiresTconSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirestconspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiresTconSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
